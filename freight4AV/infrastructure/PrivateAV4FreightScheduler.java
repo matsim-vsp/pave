0a63738cfb4f81f71e05740542881aa8769c9412
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2018-11-12 16:42:51 +0100

    appending an empty drive after dropOff. destination = some hardcoded link

---
+++ freight4AV/infrastructure/PrivateAV4FreightScheduler.java
@@ -0,0 +1,127 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2017 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package freight4AV.infrastructure;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.contrib.dvrp.data.Fleet;
+import org.matsim.contrib.dvrp.data.Vehicle;
+import org.matsim.contrib.dvrp.path.VrpPath;
+import org.matsim.contrib.dvrp.path.VrpPathWithTravelData;
+import org.matsim.contrib.dvrp.path.VrpPathWithTravelDataImpl;
+import org.matsim.contrib.dvrp.path.VrpPaths;
+import org.matsim.contrib.dvrp.router.DvrpRoutingNetworkProvider;
+import org.matsim.contrib.dvrp.schedule.Schedule;
+import org.matsim.contrib.dvrp.schedule.Schedules;
+import org.matsim.contrib.dvrp.tracker.OnlineDriveTaskTracker;
+import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelTimeModule;
+import org.matsim.contrib.dvrp.util.LinkTimePair;
+import org.matsim.contrib.freight.carrier.Carriers;
+import org.matsim.contrib.taxi.optimizer.DefaultTaxiOptimizerProvider;
+import org.matsim.contrib.taxi.run.Taxi;
+import org.matsim.contrib.taxi.run.TaxiConfigGroup;
+import org.matsim.contrib.taxi.schedule.TaxiEmptyDriveTask;
+import org.matsim.contrib.taxi.scheduler.TaxiScheduler;
+import org.matsim.core.mobsim.framework.MobsimTimer;
+import org.matsim.core.router.FastAStarEuclideanFactory;
+import org.matsim.core.router.util.LeastCostPathCalculator;
+import org.matsim.core.router.util.TravelDisutility;
+import org.matsim.core.router.util.TravelTime;
+
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
+
+/**
+ * @author jbischoff
+ *
+ */
+public class PrivateAV4FreightScheduler extends TaxiScheduler {
+
+	private Link DEPOT_LINK;
+	private LeastCostPathCalculator router;
+	private TravelTime travelTime;
+	
+
+	/**
+	 * @param taxiCfg
+	 * @param network
+	 * @param fleet
+	 * @param timer
+	 * @param params
+	 * @param travelTime
+	 * @param travelDisutility
+	 */
+	@Inject
+	public PrivateAV4FreightScheduler(TaxiConfigGroup taxiCfg, @Taxi Fleet fleet, @Named(DvrpRoutingNetworkProvider.DVRP_ROUTING) Network network,
+			MobsimTimer timer, @Named(DvrpTravelTimeModule.DVRP_ESTIMATED) TravelTime travelTime,
+			@Named(DefaultTaxiOptimizerProvider.TAXI_OPTIMIZER) TravelDisutility travelDisutility) {
+		super(taxiCfg, fleet, network, timer, travelTime, travelDisutility);
+		DEPOT_LINK = network.getLinks().get(Id.createLinkId("560"));
+		
+		//the router and traveltime variables are initialized the same way in the super class; but the fields are set to private over there. Since i'm not working on a snapshot, i can't change
+		//this. So i choose the sloppy version to have another instance of the router and the travel time field variable
+		router = new FastAStarEuclideanFactory(taxiCfg.getAStarEuclideanOverdoFactor()).createPathCalculator(network,
+				travelDisutility, travelTime);
+		this.travelTime = travelTime;
+	}
+
+	public void moveIdleVehicle(Vehicle vehicle, VrpPathWithTravelData vrpPath) {
+		Schedule schedule = vehicle.getSchedule();
+		divertOrAppendDrive(schedule, vrpPath);
+		appendStayTask(vehicle);
+	}
+
+	public void stopCruisingVehicle(Vehicle vehicle) {
+		if (!taxiCfg.isVehicleDiversion()) {
+			throw new RuntimeException("Diversion must be on");
+		}
+
+		Schedule schedule = vehicle.getSchedule();
+		TaxiEmptyDriveTask driveTask = (TaxiEmptyDriveTask)Schedules.getNextToLastTask(schedule);
+		schedule.removeLastTask();
+		OnlineDriveTaskTracker tracker = (OnlineDriveTaskTracker)driveTask.getTaskTracker();
+		LinkTimePair stopPoint = tracker.getDiversionPoint();
+		tracker.divertPath(
+				new VrpPathWithTravelDataImpl(stopPoint.time, 0, new Link[] { stopPoint.link }, new double[] { 0 }));
+
+		appendStayTask(vehicle);
+		
+	}
+	
+	@Override
+	protected void appendTasksAfterDropoff(Vehicle vehicle) {
+		addDriveToDepotTask(vehicle);
+//		appendStayTask(vehicle);
+	}
+
+	private void addDriveToDepotTask(Vehicle vehicle) {
+//		LinkTimePair beginOfIdleness = super.getEarliestIdleness(vehicle);
+		Link lastLinkInSchedule = Schedules.getLastLinkInSchedule(vehicle);
+		
+		VrpPathWithTravelData path = VrpPaths.calcAndCreatePath(Schedules.getLastLinkInSchedule(vehicle), 
+				DEPOT_LINK, Schedules.getLastTask(vehicle.getSchedule()).getEndTime(), this.router, this.travelTime);
+		
+		 vehicle.getSchedule().addTask(new TaxiEmptyDriveTask(path));
+	}
+}
---
+++ freight4AV/run/RunPrivateAVScenario.java
@@ -0,0 +1,107 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2018 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+/**
+ * 
+ */
+package freight4AV.run;
+
+import java.util.Collections;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.dvrp.passenger.PassengerEngineQSimModule;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.taxi.optimizer.TaxiOptimizer;
+import org.matsim.contrib.taxi.run.TaxiConfigConsistencyChecker;
+import org.matsim.contrib.taxi.run.TaxiConfigGroup;
+import org.matsim.contrib.taxi.run.TaxiQSimModule;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup.SnapshotStyle;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
+import org.matsim.core.scenario.ScenarioUtils;
+import freight4AV.infrastructure.*;
+
+/**
+ * @author tschlenther
+ *
+ */
+public class RunPrivateAVScenario {
+
+	public static final String CONFIG_FILE_RULEBASED = "C:/TU Berlin/MasterArbeit/input/mielec_taxi_config_rulebased.xml";
+	public static final String CONFIG_FILE_ASSIGNMENT = "C:/TU Berlin/MasterArbeit/input/mielec_taxi_config_assigment.xml";
+	
+	/**
+	 * 
+	 */
+	public RunPrivateAVScenario() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		TaxiConfigGroup taxiCfg = new TaxiConfigGroup();
+		taxiCfg.setDestinationKnown(true);
+		
+		Config config = ConfigUtils.loadConfig(CONFIG_FILE_RULEBASED, new DvrpConfigGroup(), taxiCfg);
+		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
+		
+		config.controler().setLastIteration(0);
+		config.controler().setOutputDirectory("output/testOhneStayTask/");
+//		config.qsim().setStartTime(0);
+//		config.qsim().setSimStarttimeInterpretation(StarttimeInterpretation.onlyUseStarttime);
+		config.qsim().setSnapshotStyle(SnapshotStyle.withHoles);
+//		config.global().setNumberOfThreads(8);
+		config.addConfigConsistencyChecker(new TaxiConfigConsistencyChecker());
+		config.checkConsistency();
+		
+		final Scenario scenario = ScenarioUtils.loadScenario(config);
+		Controler controler = new Controler(scenario);
+
+		controler.addOverridingModule(new PrivateFreightAVModule(scenario));
+		controler.addQSimModule(new FreightAVQSimModule(TSPrivateAVOptimizerProvider.class));
+//		controler.addQSimModule(new PassengerEngineQSimModule(taxiCfg.getMode()));
+		
+		
+		controler.addOverridingModule(DvrpModule.createModule(taxiCfg.getMode(),
+				Collections.singleton(TaxiOptimizer.class)));
+		
+	
+        
+        
+//		TSPrivateAVFleetGenerator  fleet = new TSPrivateAVFleetGenerator(scenario);  
+//        controler.addOverridingModule(new AbstractModule() {
+//			
+//			@Override
+//			public void install() {
+//				bind(org.matsim.contrib.dvrp.data.Fleet.class).annotatedWith(Names.named(taxiCfg.getMode())).toInstance(fleet);
+//			}
+//		});
+	
+        controler.run();
+	}
+	
+	
+
+}
