dffa0dfb0ab4078c1ad09296b4ad16724b78f9f7
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2019-02-12 15:45:42 +0100

    towards new module structure

--- privateAV/run/RunPFAVScenario.java
+++ privateAV/run/RunPFAVScenario.java
@@ -16,41 +16,23 @@
  *                                                                         *
  * *********************************************************************** */
 
-/**
- * 
- */
 package privateAV.run;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Collections;
 import java.util.Date;
-
-import org.apache.log4j.helpers.DateTimeDateFormat;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.contrib.dvrp.run.DvrpModule;
-import org.matsim.contrib.dvrp.run.MobsimTimerProvider;
-import org.matsim.contrib.dvrp.trafficmonitoring.DvrpTravelDisutilityProvider;
-import org.matsim.contrib.taxi.optimizer.DefaultTaxiOptimizerProvider;
-import org.matsim.contrib.taxi.optimizer.TaxiOptimizer;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
 import org.matsim.contrib.taxi.run.TaxiConfigConsistencyChecker;
 import org.matsim.contrib.taxi.run.TaxiConfigGroup;
-import org.matsim.contrib.taxi.scheduler.TaxiScheduleInquiry;
+import org.matsim.contrib.taxi.run.TaxiModule;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.QSimConfigGroup.SnapshotStyle;
 import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy.OverwriteFileSetting;
-import org.matsim.core.mobsim.framework.MobsimTimer;
 import org.matsim.core.scenario.ScenarioUtils;
-
-import privateAV.PFAVScheduler;
-import privateAV.modules.PFAVQSimModule;
-import privateAV.modules.PFAVModule;
-import privateAV.optimizer.PFAVOptimizer;
-import privateAV.optimizer.PFAVProvider;
 
 /**
  * @author tschlenther
@@ -61,7 +43,6 @@ public class RunPFAVScenario {
 	public static final String CONFIG_FILE_RULEBASED = "input/Scenarios/mielec/mielec_taxi_config_rulebased.xml";
 	public static final String CONFIG_FILE_ASSIGNMENT = "/input/Scenarios/mielec/mielec_taxi_config_assigment.xml";
 	public static final String CARRIERS_FILE = "input/Scenarios/mielec/freight/carrierPlans_routed.xml";
-	
 	public static final String OUTPUT_DIR = "output/test/" + new SimpleDateFormat("YYYY-MM-dd_HH.mm").format(new Date()) + "/";
 
 	/**
@@ -72,54 +53,81 @@ public class RunPFAVScenario {
 		
 		String pattern = "yyyy-MM-dd";
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
-
 		String date = simpleDateFormat.format(new Date());	
+		
 		TaxiConfigGroup taxiCfg = new TaxiConfigGroup();
 		
 		/*
 		 * very important: we assume that destination of trips are known in advance.
 		 * that leads to the occupiedDriveTask and the TaxiDropoffTask to be inserted at the same time as the PickUpTask (when the request gets scheduled).
-		 * in our scenario, this is realistic, since users must have defined their working location before the agreed on having their AV making freight trips.
+		 * in our scenario, this is realistic, since users must have defined their working location before the agreement on having their AV make freight trips.
 		 * 
 		 */
-		
 		taxiCfg.setDestinationKnown(true);
 		
 		Config config = ConfigUtils.loadConfig(CONFIG_FILE_RULEBASED, new DvrpConfigGroup(), taxiCfg);
 		config.controler().setOverwriteFileSetting(OverwriteFileSetting.deleteDirectoryIfExists);
-		
-		config.controler().setLastIteration(0);
+		config.controler().setLastIteration(1);
 		config.controler().setOutputDirectory(OUTPUT_DIR);
 		
-//		config.plans().setInputFile("input/Scenarios/mielec/plans_only_taxi_ONEAGENT.xml");
 		
-//		config.qsim().setStartTime(0);
-//		config.qsim().setSimStarttimeInterpretation(StarttimeInterpretation.onlyUseStarttime);
-		config.qsim().setSnapshotStyle(SnapshotStyle.withHoles);
-//		config.global().setNumberOfThreads(8);
 		config.addConfigConsistencyChecker(new TaxiConfigConsistencyChecker());
 		config.checkConsistency();
-		
-		final Scenario scenario = ScenarioUtils.loadScenario(config);
+		String mode = taxiCfg.getMode();
+
+		// load scenario
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		// setup controler
 		Controler controler = new Controler(scenario);
+		controler.addOverridingModule(new DvrpModule());
+		controler.addOverridingModule(new TaxiModule());
+		controler.configureQSimComponents(DvrpQSimComponents.activateModes(mode));
 
-//		controler.addQSimModule(new FreightAVQSimModule(TSPrivateAVOptimizerProvider.class,PrivateAV4FreightScheduler.class));
-		
-//		controler.addQSimModule(new PFAVQSimModule(PFAVProvider.class,PFAVScheduler.class));
-		//null für direktes binden des optimizers ohne provider klasse
-		
-		controler.addQSimModule(new PFAVQSimModule(null,PFAVScheduler.class));
 
-		controler.addOverridingModule(new PFAVModule(scenario));
-		//if PFAVProvider is used, we need to bind our optimizer as controller listener
-		
-//		controler.addQSimModule(new PassengerEngineQSimModule(taxiCfg.getMode()));
+		// run simulation
+		controler.run();
 		
 		
-		controler.addOverridingModule(DvrpModule.createModule(taxiCfg.getMode(),
-				Collections.singleton(TaxiOptimizer.class)));
-			
-        controler.run();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+////		config.plans().setInputFile("input/Scenarios/mielec/plans_only_taxi_ONEAGENT.xml");
+//		
+////		config.qsim().setStartTime(0);
+////		config.qsim().setSimStarttimeInterpretation(StarttimeInterpretation.onlyUseStarttime);
+//		config.qsim().setSnapshotStyle(SnapshotStyle.withHoles);
+////		config.global().setNumberOfThreads(8);
+//		config.addConfigConsistencyChecker(new TaxiConfigConsistencyChecker());
+//		config.checkConsistency();
+//		
+//		final Scenario scenario = ScenarioUtils.loadScenario(config);
+//		Controler controler = new Controler(scenario);
+//
+////		controler.addQSimModule(new FreightAVQSimModule(TSPrivateAVOptimizerProvider.class,PrivateAV4FreightScheduler.class));
+//		
+////		controler.addQSimModule(new PFAVQSimModule(PFAVProvider.class,PFAVScheduler.class));
+//		//null für direktes binden des optimizers ohne provider klasse
+//		
+//		controler.addQSimModule(new PFAVQSimModule(null,PFAVScheduler.class));
+//
+//		controler.addOverridingModule(new PFAVModule(scenario));
+//		//if PFAVProvider is used, we need to bind our optimizer as controller listener
+//		
+////		controler.addQSimModule(new PassengerEngineQSimModule(taxiCfg.getMode()));
+//		
+//		
+//		controler.addOverridingModule(DvrpModule.createModule(taxiCfg.getMode(),
+//				Collections.singleton(TaxiOptimizer.class)));
+//			
+//        controler.run();
 	}
 	
 	
