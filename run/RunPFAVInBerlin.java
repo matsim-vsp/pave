61958a82586acf6b27e7ad863e8334af0dbc23c4
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2019-03-02 11:07:36 +0100

    RunPFAVInBerlin class

---
+++ run/RunPFAVInBerlin.java
@@ -0,0 +1,141 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package run;
+
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.contrib.taxi.run.TaxiConfigConsistencyChecker;
+import org.matsim.contrib.taxi.run.TaxiConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.gbl.MatsimRandom;
+import org.matsim.run.RunBerlinScenario;
+import privateAV.modules.PFAVModeModule;
+import privateAV.modules.PFAVQSimModule;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class RunPFAVInBerlin {
+
+    private static final String CONFIG_v53_1pct = "input/BerlinScenario/5.3/berlin-v5.3-1pct.config.xml";
+    private static final String OUTPUTDIR = "output/Berlin/test/" + new SimpleDateFormat("YYYY-MM-dd_HH.mm").format(new Date()) + "/";
+    private static final String CARRIERS_FILE = "input/FrachtNachfrage/KEP/PFAVScenario/test_onlyOneCarrier.xml";
+    private static final String VEHTYPES_FILE = "input/FrachtNachfrage/KEP/PFAVScenario/baseCaseVehicleTypes.xml";
+    private static final int LAST_ITERATION = 0;
+    private static final double PERCENTAGE_OF_PFAV_OWNERS = 0.05;
+    private static final double DOWN_SAMPLE_SIZE = 0.1;
+    private static Logger log = Logger.getLogger(RunPFAVInBerlin.class);
+    private static Set<Id<Person>> PFAV_owners = new HashSet<>();
+
+
+    public static void main(String[] args) {
+
+        RunBerlinScenario berlin = new RunBerlinScenario(CONFIG_v53_1pct, null);
+
+        Config config = berlin.prepareConfig();
+
+        TaxiConfigGroup taxiCfg = new TaxiConfigGroup();
+        taxiCfg.setBreakSimulationIfNotAllRequestsServed(true);
+        /*
+         * very important: we assume that destinations of trips are known in advance.
+         * that leads to the occupiedDriveTask and the TaxiDropoffTask to be inserted at the same time as the PickUpTask (when the request gets scheduled).
+         * in our scenario, this is realistic, since users must have defined their working location before the agreement on having their AV make freight trips.
+         *
+         */
+        taxiCfg.setDestinationKnown(true);
+        taxiCfg.setPickupDuration(120);
+        taxiCfg.setDropoffDuration(60);
+        taxiCfg.setTaxisFile("something");
+        config.addModule(taxiCfg);
+        String mode = taxiCfg.getMode();
+
+        config.addModule(new DvrpConfigGroup());
+
+        config.strategy().setFractionOfIterationsToDisableInnovation(1);        //  ???
+
+        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+        config.controler().setLastIteration(LAST_ITERATION);
+        config.controler().setOutputDirectory(OUTPUTDIR);
+
+        config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+        config.qsim().setNumberOfThreads(1);
+
+        config.addConfigConsistencyChecker(new TaxiConfigConsistencyChecker());
+        config.checkConsistency();
+
+        Scenario scenario = berlin.prepareScenario();
+
+        log.warn("number of persons : " + scenario.getPopulation().getPersons().size());
+        convertAgentsToPFAVOwners(scenario);
+        log.warn("number of PFAV owners : " + PFAV_owners.size());
+        downsample(scenario.getPopulation().getPersons(), DOWN_SAMPLE_SIZE);
+
+        // setup controler
+        Controler controler = berlin.prepareControler(new DvrpModule());
+        controler.addOverridingModule(new AbstractModule() {
+            @Override
+            public void install() {
+                install(new PFAVModeModule(taxiCfg, scenario, CARRIERS_FILE, VEHTYPES_FILE));
+                installQSimModule(new PFAVQSimModule(taxiCfg));
+            }
+        });
+        controler.configureQSimComponents(DvrpQSimComponents.activateModes(mode));
+
+        // run simulation
+        berlin.run();
+
+    }
+
+
+    public static void convertAgentsToPFAVOwners(Scenario scenario) {
+        log.info("start converting car legs to taxi legs with the probability " + PERCENTAGE_OF_PFAV_OWNERS);
+        final Random rnd = MatsimRandom.getLocalInstance();
+        for (Person p : scenario.getPopulation().getPersons().values()) {
+            if (rnd.nextDouble() <= PERCENTAGE_OF_PFAV_OWNERS) {
+                //TODO: i only care about the selected plans, right?
+                PFAV_owners.add(p.getId());
+                for (PlanElement pe : p.getSelectedPlan().getPlanElements()) {
+                    if (pe instanceof Leg) {
+                        ((Leg) pe).setMode("taxi");
+                    }
+                }
+            }
+        }
+    }
+
+    private static void downsample(final Map map, final double sample) {
+        final Random rnd = MatsimRandom.getLocalInstance();
+        log.warn("map size before=" + map.size());
+        map.values().removeIf(person -> rnd.nextDouble() > sample && !PFAV_owners.contains(((Person) person).getId()));
+        log.warn("map size after=" + map.size());
+    }
+}
