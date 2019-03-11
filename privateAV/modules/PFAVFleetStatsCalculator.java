cc601a9c0305cb8979e7f36bc25fa10131d5617e
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2019-03-11 15:42:14 +0100

    calculate must return times based on activity end times before mode legs

--- privateAV/modules/PFAVFleetStatsCalculator.java
+++ privateAV/modules/PFAVFleetStatsCalculator.java
@@ -7,7 +7,7 @@ import org.matsim.api.core.v01.population.*;
 import org.matsim.contrib.dvrp.fleet.DvrpVehicle;
 import org.matsim.contrib.dvrp.fleet.Fleet;
 import org.matsim.contrib.dvrp.fleet.FleetSpecification;
-import org.matsim.contrib.dvrp.fleet.FleetStatsCalculator;
+import org.matsim.contrib.dvrp.run.QSimScopeObjectListener;
 import org.matsim.core.controler.events.BeforeMobsimEvent;
 import org.matsim.core.controler.listener.BeforeMobsimListener;
 import privateAV.PFAVUtils;
@@ -15,12 +15,13 @@ import privateAV.vehicle.PFAVSpecification;
 
 import java.util.*;
 
-public class PFAVFleetStatsCalculator implements FleetStatsCalculator, BeforeMobsimListener {
+public class PFAVFleetStatsCalculator implements QSimScopeObjectListener<Fleet>, BeforeMobsimListener {
 
     private final FleetSpecification fleetSpecification;
     private final Population population;
     private Set<Id<DvrpVehicle>> oldVehicles = new HashSet<Id<DvrpVehicle>>();
     private final String mode;
+    private Fleet fleet;
 
     public PFAVFleetStatsCalculator(FleetSpecification fleetSpecification, Scenario scenario, String mode){
         this.fleetSpecification = fleetSpecification;
@@ -30,11 +31,27 @@ public class PFAVFleetStatsCalculator implements FleetStatsCalculator, BeforeMob
 
     @Override
     public void notifyBeforeMobsim(BeforeMobsimEvent event) {
+        List<PFAVSpecification> vehiclesForIteration = determinePFAVsForIteration();
+        for (PFAVSpecification sp : vehiclesForIteration) {
+            if (oldVehicles.contains(sp.getId())) {
+                fleetSpecification.replaceVehicleSpecification(sp);
+                oldVehicles.remove(sp.getId());
+            } else {
+                fleetSpecification.addVehicleSpecification(sp);
+            }
+        }
+        //delete all vehicle specifications that are no longer needed (agents try out different mode)
+        for (Id<DvrpVehicle> id : oldVehicles) {
+            fleetSpecification.removeVehicleSpecification(id);
+        }
+    }
+
+    private List<PFAVSpecification> determinePFAVsForIteration() {
         List<PFAVSpecification> vehiclesForIteration = new ArrayList<>();
         for(Person p : this.population.getPersons().values()) {
-
             Id<Link> vehicleStartLink = null;
-            Queue<Double>  actEndTimesAfterModeLegs = new LinkedList<>();
+            Queue<Double> actEndTimesAfterModeLegs = new LinkedList<>();
+            Queue<Double> actEndTimesBeforeModeLegs = new LinkedList<>();
 
 
             //TODO: test this
@@ -45,34 +62,25 @@ public class PFAVFleetStatsCalculator implements FleetStatsCalculator, BeforeMob
                     if (((Leg) pe).getMode().equals(mode)) {
                         if(vehicleStartLink == null) vehicleStartLink = ((Leg) pe).getRoute().getStartLinkId();
                         actEndTimesAfterModeLegs.add(getActivityEndTimeOfNextNonStageActivity(plan, i));
+                        actEndTimesBeforeModeLegs.add(getActivityEndTimeOfPreviousNonStageActivity(plan, i));
                     }
                 }
             }
             if (vehicleStartLink != null) {
-                Id<DvrpVehicle> vehicleId = Id.create(p.getId().toString() + "_av", DvrpVehicle.class);
+                Id<DvrpVehicle> vehicleId = Id.create(p.getId().toString() + PFAVUtils.PFAV_ID_SUFFIX, DvrpVehicle.class);
                 PFAVSpecification specification = PFAVSpecification.newBuilder()
                         .serviceBeginTime(0.)
                         .serviceEndTime(36 * 3600)
                         .startLinkId(vehicleStartLink)
                         .id(vehicleId)
                         .capacity(PFAVUtils.DEFAULT_PFAV_CAPACITY)
-                        .actEndTimes(actEndTimesAfterModeLegs)
+//                        .actEndTimes(actEndTimesAfterModeLegs)
+                        .actEndTimes(actEndTimesBeforeModeLegs)
                         .build();
                 vehiclesForIteration.add(specification);
             }
         }
-        for(PFAVSpecification sp : vehiclesForIteration){
-            if(oldVehicles.contains(sp.getId())){
-                fleetSpecification.replaceVehicleSpecification(sp);
-                oldVehicles.remove(sp.getId());
-            } else {
-                fleetSpecification.addVehicleSpecification(sp);
-            }
-        }
-        //delete all vehicle specifications that are no longer needed (agents try out different mode)
-        for(Id<DvrpVehicle> id : oldVehicles){
-            fleetSpecification.removeVehicleSpecification(id);
-        }
+        return vehiclesForIteration;
     }
 
     private double getActivityEndTimeOfNextNonStageActivity(Plan plan, int startIndex) {
@@ -104,21 +112,38 @@ public class PFAVFleetStatsCalculator implements FleetStatsCalculator, BeforeMob
         return Double.NEGATIVE_INFINITY;
     }
 
+    private double getActivityEndTimeOfPreviousNonStageActivity(Plan plan, int startIndex) {
+        Leg lastLeg = null;
+        for (int i = startIndex; i >= 0; i--) {
+            PlanElement pe = plan.getPlanElements().get(i);
+            if (pe instanceof Leg)
+                lastLeg = (Leg) pe;
+            if (pe instanceof Activity) {
+                //filter stage activities - TODO: this can be surely done more properly but works so far..
+//                TODO: should we allow stage activities for PFAV owner in the first place? or delete acces and egress walks straight from the plans?
+                if (!((Activity) pe).getType().contains("interaction")) {
+                    //activity could be something like leisure in open berlin scenario which does not have an end time set
+                    Activity act = (Activity) pe;
+                    if (act.getEndTime() == Double.NEGATIVE_INFINITY) {
+                        return lastLeg.getDepartureTime();
+                    } else {
+                        return act.getEndTime();
+                    }
+                }
+            }
+        }
+        throw new IllegalStateException("could not compute must return time for agent " + plan.getPerson().getId() + " before leg " + plan.getPlanElements().get(startIndex));
+    }
+
+//    @Override
+//    public void updateStats(Fleet fleet) {
+//        oldVehicles.clear();
+//        oldVehicles.addAll(fleet.getVehicles().keySet());
+//    }
+
     @Override
-    public void updateStats(Fleet fleet) {
-        oldVehicles.clear();
-        oldVehicles.addAll(fleet.getVehicles().keySet());
-//        oldVehicles = fleet.getVehicles()
-//                .values()
-//                .stream()
-//                .map(v -> ImmutableDvrpVehicleSpecification.newBuilder()
-//                        .id(v.getId())
-//                        .startLinkId(Schedules.getLastLinkInSchedule(v).getId())
-//                        .capacity(v.getCapacity())
-//                        .serviceBeginTime(v.getServiceBeginTime())
-//                        .serviceEndTime(v.getServiceEndTime())
-//                        .build())
-//                .collect(Collectors.toList());
+    public void objectCreated(Fleet fleet) {
+        this.fleet = fleet;
     }
 
 }
