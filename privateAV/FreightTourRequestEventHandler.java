1550dbec3e50e0581bc591e05ea3c11b9d354aa5
Tilmann Schlenther <schlenther@vsp.tu-berlin.de>
2019-10-16 17:40:36 +0200

    fix failing test - make interface FreightTourRequestEventHandler public again

--- privateAV/FreightTourRequestEventHandler.java
+++ privateAV/FreightTourRequestEventHandler.java
@@ -2,7 +2,7 @@ package privateAV;
 
 import org.matsim.core.events.handler.EventHandler;
 
-interface FreightTourRequestEventHandler extends EventHandler {
+public interface FreightTourRequestEventHandler extends EventHandler {
     void handleEvent(final EventFreightTourScheduled event);
 
     void handleEvent(final EventFreightTourRequestRejected event);
