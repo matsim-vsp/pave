dad9f45d32830e07618a5a500df7265616b105e3
tschlenther <schlenther@vsp.tu-berlin.de>
2020-02-28 19:18:27 +0100

    tourTasks of blocking are contained in a list instead of queue

--- src/main/java/org/matsim/drt/DrtBlockingRequest.java
+++ src/main/java/org/matsim/drt/DrtBlockingRequest.java
@@ -1,11 +1,10 @@
 package org.matsim.drt;
 
 import org.matsim.api.core.v01.Id;
-import org.matsim.api.core.v01.Identifiable;
 import org.matsim.contrib.dvrp.optimizer.Request;
 import org.matsim.contrib.dvrp.schedule.Task;
 
-import java.util.PriorityQueue;
+import java.util.List;
 
 class DrtBlockingRequest implements Request {
 
@@ -14,14 +13,27 @@ class DrtBlockingRequest implements Request {
     private final double startTime;
     private final double submissionTime;
 
-    private PriorityQueue<Task> tasks;
+    private List<Task> tasks;
 
-    DrtBlockingRequest(final Id<Request> id, final double submissionTime, final double startTime, final double endTime, PriorityQueue<Task> dvrpVehicleTasks) {
+    DrtBlockingRequest(final Id<Request> id, final double submissionTime, final double startTime, final double endTime, List<Task> dvrpVehicleTasks) {
         this.id = id;
         this.submissionTime = submissionTime;
         this.startTime = startTime;
         this.endTime = endTime;
         this.tasks = dvrpVehicleTasks;
+        checkStartAndEndTimeConsistency();
+    }
+
+    private void checkStartAndEndTimeConsistency() {
+        if(this.startTime > this.tasks.get(0).getBeginTime()){
+            throw new IllegalArgumentException("start time of the blocking should be less than or equal to the begin time of the first corresponding task");
+        }
+        if(this.endTime < this.tasks.get(this.tasks.size() -1).getEndTime()){
+            throw new IllegalArgumentException("end time of the blocking should be greater than or equal to the end time of the last corresponding task");
+        }
+        if(this.submissionTime > this.startTime){
+            throw new IllegalArgumentException("submission time should be less than or equal to startTime");
+        }
     }
 
     @Override
@@ -42,7 +54,7 @@ class DrtBlockingRequest implements Request {
         return this.startTime;
     }
 
-    PriorityQueue<Task> getTasks() {
+    List<Task> getTasks() {
         return this.tasks;
     }
 }
