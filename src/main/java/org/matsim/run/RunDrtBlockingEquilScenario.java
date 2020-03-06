0b454642d6e6428bd53d7b205155a170d84cb115
tschlenther <schlenther@vsp.tu-berlin.de>
2020-03-06 15:43:38 +0100

    equil test scenario for drt blockings

---
+++ src/main/java/org/matsim/run/RunDrtBlockingEquilScenario.java
@@ -0,0 +1,149 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.run;
+
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PopulationFactory;
+import org.matsim.contrib.drt.routing.DrtRoute;
+import org.matsim.contrib.drt.routing.DrtRouteFactory;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.DrtModeModule;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpModule;
+import org.matsim.contrib.dvrp.run.DvrpQSimComponents;
+import org.matsim.contrib.freight.FreightConfigGroup;
+import org.matsim.contrib.freight.carrier.Carrier;
+import org.matsim.contrib.freight.carrier.CarrierUtils;
+import org.matsim.contrib.freight.utils.FreightUtils;
+import org.matsim.contrib.otfvis.OTFVisLiveModule;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.QSimConfigGroup;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.misc.Time;
+import org.matsim.drt.DrtBlockingQSimModule;
+
+import javax.management.InvalidAttributeValueException;
+
+public class RunDrtBlockingEquilScenario {
+
+    public static void main(String[] args) {
+
+        Config config = ConfigUtils.loadConfig("scenarios/equil/config.xml");
+        config.controler().setOutputDirectory("./output/equil/drtBlocking/");
+        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles);
+        config.controler().setLastIteration(0);
+
+        DrtConfigGroup drtCfg = prepareDrtConfig(config);
+
+        FreightConfigGroup freightCfg = ConfigUtils.addOrGetModule(config, FreightConfigGroup.class);
+        freightCfg.setCarriersFile("equil_carriers.xml");
+        freightCfg.setCarriersVehicleTypesFile("equil_carrierVehicleTypes.xml");
+
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+        FreightUtils.loadCarriersAccordingToFreightConfig(scenario);
+
+
+        try {
+            FreightUtils.runJsprit(scenario, freightCfg);
+        } catch (InvalidAttributeValueException e) {
+            e.printStackTrace();
+        }
+
+        setupPopulation(scenario);
+
+        Controler controler = new Controler(scenario);
+        configureControler(drtCfg, controler);
+
+
+        controler.run();
+    }
+
+    private static DrtConfigGroup prepareDrtConfig(Config config) {
+        ConfigUtils.addOrGetModule(config, DvrpConfigGroup.class);
+
+        MultiModeDrtConfigGroup multiModeDrtConfigGroup = ConfigUtils.addOrGetModule(config, MultiModeDrtConfigGroup.class);
+
+        DrtConfigGroup drtCfg = new DrtConfigGroup();//DrtConfigGroup.getSingleModeDrtConfig(config);
+        drtCfg.setMode(TransportMode.drt);
+        drtCfg.setMaxWaitTime(2 * 3600);
+        drtCfg.setMaxTravelTimeAlpha(1);
+        drtCfg.setMaxTravelTimeBeta(15 * 60);
+        drtCfg.setStopDuration(60);
+        drtCfg.setEstimatedDrtSpeed(27.78);
+        drtCfg.setVehiclesFile("drtVehicles.xml");
+        config.qsim().setSimStarttimeInterpretation(QSimConfigGroup.StarttimeInterpretation.onlyUseStarttime);
+        multiModeDrtConfigGroup.addParameterSet(drtCfg);
+        return drtCfg;
+    }
+
+    private static void configureControler(DrtConfigGroup drtCfg, Controler controler) {
+        controler.addOverridingModule( new DvrpModule() ) ;
+        controler.addOverridingModule( new DrtModeModule(drtCfg) ) ;
+        controler.addOverridingQSimModule( new DrtBlockingQSimModule(drtCfg));
+
+        controler.configureQSimComponents( DvrpQSimComponents.activateModes( TransportMode.drt ) ) ;
+    }
+
+
+    private static void setupPopulation(Scenario scenario){
+        scenario.getPopulation()
+                .getFactory()
+                .getRouteFactories()
+                .setRouteFactory(DrtRoute.class, new DrtRouteFactory());
+        
+        //delete the old population
+        scenario.getPopulation().getPersons().clear();
+
+        Id<Link> homeLink = Id.createLinkId(1);
+        Id<Link> workLink = Id.createLinkId(22);
+
+        PopulationFactory popFactory = scenario.getPopulation().getFactory();
+
+        for (int i = 1; i <= 48; i++) {
+            Person person = popFactory.createPerson(Id.createPersonId("person_" + i));
+            Plan plan = popFactory.createPlan();
+
+            Activity home = popFactory.createActivityFromLinkId("h", homeLink);
+            home.setEndTime(i * 1800);
+            plan.addActivity(home);
+
+            plan.addLeg(popFactory.createLeg("drt"));
+
+            Activity work = popFactory.createActivityFromLinkId("w", workLink);
+            plan.addActivity(work);
+
+            person.addPlan(plan);
+            scenario.getPopulation().addPerson(person);
+        }
+
+    }
+
+}
