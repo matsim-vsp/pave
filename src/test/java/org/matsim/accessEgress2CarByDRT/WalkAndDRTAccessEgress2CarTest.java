85064fc924711f1e3997c85301d1980d06ba1912
tschlenther <schlenther@vsp.tu-berlin.de>
2020-06-24 15:20:37 +0200

    prepare carDrtCar

--- src/test/java/org/matsim/accessEgress2CarByDRT/WalkAndDRTAccessEgress2CarTest.java
+++ src/test/java/org/matsim/accessEgress2CarByDRT/WalkAndDRTAccessEgress2CarTest.java
@@ -3,6 +3,7 @@ package org.matsim.accessEgress2CarByDRT;
 import com.google.common.collect.ImmutableSet;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.drt.run.DrtConfigGroup;
@@ -12,16 +13,22 @@ import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.QSimConfigGroup;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.examples.ExamplesUtils;
 import org.matsim.testcases.MatsimTestUtils;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleUtils;
 import org.matsim.vis.otfvis.OTFVisConfigGroup;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class WalkAndDRTAccessEgress2CarTest {
@@ -59,16 +66,32 @@ public class WalkAndDRTAccessEgress2CarTest {
 		config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
 		config.controler().setOutputDirectory(utils.getOutputDirectory() + "carPop");
 
-		//add modeParams
-		config.planCalcScore().addModeParams(new PlanCalcScoreConfigGroup.ModeParams("walkCarDrt"));
-		config.planCalcScore().addModeParams(new PlanCalcScoreConfigGroup.ModeParams("drtCarWalk"));
+		{ //add scoring parameters for new modes
+			config.planCalcScore().addModeParams(new PlanCalcScoreConfigGroup.ModeParams("walkCarDrt"));
+			config.planCalcScore().addModeParams(new PlanCalcScoreConfigGroup.ModeParams("drtCarWalk"));
+			config.planCalcScore().addModeParams(new PlanCalcScoreConfigGroup.ModeParams("carDrtCar"));
+			config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("walkCarDrt interaction").setScoringThisActivityAtAll(false));
+			config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("drtCarWalk interaction").setScoringThisActivityAtAll(false));
+			config.planCalcScore().addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams("carDrtCar interaction").setScoringThisActivityAtAll(false));
+		}
 
-		//this is the wrong way around (create controler before manipulating scenario...
+		//this is the wrong way around (create controler before manipulating scenario, but it is handy here...
 		Controler controler = DrtControlerCreator.createControler(config, false);
-//		setupPopulation(controler.getScenario().getPopulation());
+
+		{ //add mode vehicle id's
+			controler.getScenario().getPopulation().getPersons().values().parallelStream().forEach(person -> {
+				Map<String, Id<Vehicle>> vehicleIdMap = new HashMap<>();
+				Id<Vehicle> vehicleId = Id.createVehicleId(person.getId().toString());
+				vehicleIdMap.put("walkCarDrt", vehicleId);
+				vehicleIdMap.put("drtCarWalk", vehicleId);
+				vehicleIdMap.put("carDrtcar", vehicleId);
+				VehicleUtils.insertVehicleIdsIntoAttributes(person, vehicleIdMap);
+			});
+		}
 
 		controler.addOverridingModule(new WalkAccessDRTEgress2CarModule("walkCarDrt", drtCfg));
 		controler.addOverridingModule(new DRTAccessWalk2Egress2CarModule("drtCarWalk", drtCfg));
+//		controler.addOverridingModule(new CarAccessEgress2DrtModule("carDrtCar", drtCfg));
 
 		controler.run();
 	}
@@ -82,11 +105,17 @@ public class WalkAndDRTAccessEgress2CarTest {
 
 		network.getLinks().values().forEach(link -> {
 
+			link.setLength(300);
+			link.setFreespeed(15.0);
+
 			Set<String> modes = new HashSet<>();
 			modes.addAll(link.getAllowedModes());
 
 			if(modes.contains(TransportMode.walk) ) modes.add("drt");
-
+			else if (modes.contains(TransportMode.car)){
+				modes.add("walkCarDrt");
+				modes.add("drtCarWalk");
+			}
 			modes.add(TransportMode.walk);
 			link.setAllowedModes(modes);
 
