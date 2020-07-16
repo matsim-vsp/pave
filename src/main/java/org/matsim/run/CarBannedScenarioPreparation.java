c4467ce59b0a5ee4cbe16cb362687d639fff329b
tschlenther <schlenther@vsp.tu-berlin.de>
2020-07-16 19:20:03 +0200

    carBannedScenario

---
+++ src/main/java/org/matsim/run/CarBannedScenarioPreparation.java
@@ -0,0 +1,159 @@
+/* *********************************************************************** *
+ * project: org.matsim.*
+ * Controler.java
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ * copyright       : (C) 2007 by the members listed in the COPYING,        *
+ *                   LICENSE and WARRANTY file.                            *
+ * email           : info at matsim dot org                                *
+ *                                                                         *
+ * *********************************************************************** *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *   See also COPYING, LICENSE and WARRANTY file                           *
+ *                                                                         *
+ * *********************************************************************** */
+
+package org.matsim.run;
+
+import com.google.common.collect.ImmutableSet;
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.dvrp.run.DvrpConfigGroup;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
+import org.matsim.core.network.algorithms.MultimodalNetworkCleaner;
+import org.matsim.run.drt.BerlinShpUtils;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class CarBannedScenarioPreparation {
+
+	private static Logger log = Logger.getLogger(CarBannedScenarioPreparation.class);
+
+	/**
+	 * Firstly, attempts to load the drt service area shape file form {@code drtConfigGroup}.
+	 * Then, removes TransportMode.car from the allowedModes set for each link within the drt service area and adds drtConfigGroup.getMode() instead.
+	 * Finally, runs MultiModalNetworkCleaner..
+	 * @param scenario
+	 * @param drtConfigGroup
+	 */
+	static final void banCarFromDRTServiceArea(Scenario scenario, DrtConfigGroup drtConfigGroup) {
+
+		String drtServiceAreaShapeFile = drtConfigGroup.getDrtServiceAreaShapeFile();
+		if (drtServiceAreaShapeFile == null || drtServiceAreaShapeFile.equals("") || drtServiceAreaShapeFile.equals("null")) {
+			throw new IllegalArgumentException("if you want to ban cars from the drt service area you must provide a service area shape file in the DrtConfigGroup!");
+		}
+
+		log.info("Read service area shape file.");
+		BerlinShpUtils shpUtils = new BerlinShpUtils( drtServiceAreaShapeFile );
+
+		log.info("collect car links in service area");
+		Set<Id<Link>> serviceAreaLinks = scenario.getNetwork().getLinks().values()
+				.parallelStream()
+				.filter(link -> link.getAllowedModes().contains(TransportMode.car))
+				.filter(link -> shpUtils.isCoordInDrtServiceArea(link.getFromNode().getCoord())
+						|| shpUtils.isCoordInDrtServiceArea(link.getToNode().getCoord()))
+				.map(link -> link.getId())
+				.collect(Collectors.toSet());
+
+		log.info("found " + serviceAreaLinks.size() + " car links in service area");
+
+		log.info("Start adjusting network. That means, car will be prohibited and " + drtConfigGroup.getMode() + " will be allowed. All other modes are not touched..");
+		//i am not sure whether we could integrate the following step into the procedure above, as the manipulation of allowedModes could mean interference...
+		serviceAreaLinks.parallelStream().forEach(linkId -> {
+			Link link = scenario.getNetwork().getLinks().get(linkId);
+			Set<String> allowedModes = new HashSet<>(link.getAllowedModes());
+			allowedModes.remove(TransportMode.car);
+			allowedModes.add(drtConfigGroup.getMode());
+			link.setAllowedModes(allowedModes);
+		});
+
+		Set<String> modes = new HashSet<>();
+		modes.add(drtConfigGroup.getMode());
+		log.info("clean network");
+		new MultimodalNetworkCleaner(scenario.getNetwork()).run(modes);
+		log.info("finished");
+	}
+
+	//need the specific DrtCOnfigGroup, this is why this is not converted to a proper ConfigConsistencyChecker yet...
+
+	/**
+	 *
+	 * @param config
+	 * @param drtConfigGroup the DrtConfigGroup for the drt mode which will be used in intermodal car+drt trip within the carBanned area
+	 * @param additionalModes the modes that represent the intermodal mode chains with drt as access or egress mode to car, respectively. Most typically: walkCarDrt and drtCarWalk
+	 */
+	static final void prepareConfig(Config config, DrtConfigGroup drtConfigGroup, Collection<String> additionalModes) {
+
+		if(! config.plansCalcRoute().getAccessEgressType().equals(PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLink) ||
+				config.plansCalcRoute().getAccessEgressType().equals(PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLinkPlusTimeConstant)){
+			throw new IllegalArgumentException("if you want to simulate a carBannedFromCity scenario using intermodal car+drt trips, you must configure network routing for access and egress by setting " +
+					"PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLink or PlansCalcRouteConfigGroup.AccessEgressType.accessEgressModeToLinkPlusTimeConstant!!!"
+			);
+		}
+
+
+		DvrpConfigGroup dvrpConfigGroup = DvrpConfigGroup.get(config);
+		if(! dvrpConfigGroup.getNetworkModes().contains(drtConfigGroup.getMode()) ){
+			log.warn("the drt mode " + drtConfigGroup.getMode() + " is not registered as network mode for dvrp - which is necessary in a bannedCarInDRTServiceArea scenario");
+			log.warn("adding mode " + drtConfigGroup.getMode() + " as network mode for dvrp... ");
+			dvrpConfigGroup.setNetworkModes(ImmutableSet.<String>builder()
+					.addAll(dvrpConfigGroup.getNetworkModes())
+					.add(drtConfigGroup.getMode())
+					.build());
+		}
+
+		if(! drtConfigGroup.isUseModeFilteredSubnetwork()){
+			log.warn("setting drtConfigGroup.isUseModeFilteredSubnetwork() to true...");
+			drtConfigGroup.setUseModeFilteredSubnetwork(true);
+		}
+
+		configureScoringForNewModes(config, additionalModes);
+	}
+
+	private static final void configureScoringForNewModes(Config config, Collection<String> additionalModes) {
+		log.info("configure new modes for car trips originating or destined to the banned area. will copy scoring parameters from mode TransportMode.car");
+		{
+			//add scoring parameters for new modes - copied from car
+			Map<String, PlanCalcScoreConfigGroup.ScoringParameterSet> scoringParams = config.planCalcScore().getScoringParametersPerSubpopulation();
+			scoringParams.values().forEach(scoringParameterSet -> {
+				PlanCalcScoreConfigGroup.ModeParams carParams = scoringParameterSet.getOrCreateModeParams(TransportMode.car);
+				additionalModes.forEach(additionalMode -> configureScoringForMode(config, scoringParameterSet, carParams, additionalMode));
+			});
+		}
+	}
+
+	private static final void configureScoringForMode(Config config, PlanCalcScoreConfigGroup.ScoringParameterSet scoringParameterSet, PlanCalcScoreConfigGroup.ModeParams carParams, String newMode) {
+		PlanCalcScoreConfigGroup.ModeParams pp = new PlanCalcScoreConfigGroup.ModeParams(newMode);
+		copyModeParams(carParams, pp);
+		scoringParameterSet.addModeParams(pp);
+		//add scoring params for stage activity
+		scoringParameterSet.addActivityParams(new PlanCalcScoreConfigGroup.ActivityParams(PlanCalcScoreConfigGroup.createStageActivityType(newMode))
+				.setScoringThisActivityAtAll(false));
+	}
+
+	private static final void copyModeParams(PlanCalcScoreConfigGroup.ModeParams fromModeParams, PlanCalcScoreConfigGroup.ModeParams toModeParams) {
+		toModeParams.setConstant(fromModeParams.getConstant());
+		toModeParams.setMarginalUtilityOfTraveling(fromModeParams.getMarginalUtilityOfTraveling());
+		toModeParams.setMarginalUtilityOfDistance(fromModeParams.getMarginalUtilityOfDistance());
+		toModeParams.setDailyMonetaryConstant(fromModeParams.getDailyMonetaryConstant());
+		toModeParams.setDailyUtilityConstant(fromModeParams.getDailyUtilityConstant());
+		toModeParams.setMonetaryDistanceRate(fromModeParams.getMonetaryDistanceRate());
+	}
+
+}
+
--- src/main/java/org/matsim/run/RunBerlinCarBannedFromCityScenarioWithMobilityTypesAndDrtSpeedUp.java
+++ src/main/java/org/matsim/run/RunBerlinCarBannedFromCityScenarioWithMobilityTypesAndDrtSpeedUp.java
@@ -21,24 +21,39 @@
 package org.matsim.run;
 
 import org.apache.log4j.Logger;
+import org.matsim.accessEgress2CarByDRT.DRTAccessWalkEgress2CarModule;
+import org.matsim.accessEgress2CarByDRT.WalkAccessDRTEgress2CarModule;
+import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
 import org.matsim.core.controler.Controler;
+import org.matsim.core.router.AnalysisMainModeIdentifier;
+import org.matsim.core.router.MainModeIdentifier;
 import org.matsim.drtSpeedUp.DrtSpeedUpModule;
 import org.matsim.optDRT.MultiModeOptDrtConfigGroup;
 import org.matsim.optDRT.OptDrt;
 import org.matsim.run.drt.RunDrtOpenBerlinScenario;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleUtils;
 
-public class RunBerlinScenarioWithMobilityTypesAndDrtSpeedUp {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RunBerlinCarBannedFromCityScenarioWithMobilityTypesAndDrtSpeedUp {
 
 
-    private static final Logger log = Logger.getLogger(RunBerlinScenarioWithMobilityTypesAndDrtSpeedUp.class );
+    private static final Logger log = Logger.getLogger(RunBerlinCarBannedFromCityScenarioWithMobilityTypesAndDrtSpeedUp.class );
 
-    private static final String BERLIN_V5_5_CONFIG = "scenarios/berlin-v5.5-1pct/input/drt/pave-berlin-drt-v5.5-1pct.config-transsims.xml";
+//    private static final String BERLIN_V5_5_CONFIG = "scenarios/berlin-v5.5-1pct/input/drt/pave-berlin-drt-v5.5-1pct.config-transsims.xml";
+    private static final String BERLIN_V5_5_CONFIG = "scenarios/berlin-v5.5-1pct/input/drt/berlin-drt-v5.5-1pct.config_banCarTest.xml";
 
 
-    private static final boolean BAN_CAR_FROM_DRT_SERVICE_AREA = true;
+    static final String WALK_ACCESS_DRT_EGRESS_MODE = "walkCarDrt";
+    static final String DRT_ACCESS_DRT_WALK_MODE = "drtCarWalk";
 
     /**
      * @param args array containing program arguments in the following order: global sensitivity factor for mobility types (as numeric argument), path to config, custom modules to load
@@ -52,7 +67,7 @@ public class RunBerlinScenarioWithMobilityTypesAndDrtSpeedUp {
         double sensitivityFactor = 0.1;
         String[] configArgs;
         if ( args.length==0 ) {
-            configArgs = new String[]{BERLIN_V5_5_CONFIG, "--config:controler.outputDirectory", "output/berlin5.5_10pct_pave_TEST_snapshots"};
+            configArgs = new String[]{BERLIN_V5_5_CONFIG, "--config:controler.outputDirectory", "output/berlin5.5_1pct_testBannedCarArea2"};
         } else {
             sensitivityFactor = Double.parseDouble(args[0]);
             configArgs = new String[args.length-1];
@@ -61,19 +76,20 @@ public class RunBerlinScenarioWithMobilityTypesAndDrtSpeedUp {
             }
         }
 
+        //prepare config
         Config config = RunDrtOpenBerlinScenario.prepareConfig(configArgs);
         DrtSpeedUpModule.addTeleportedDrtMode(config);
 
+        //configure pave mobility types..
         PAVEMobilityTypesForBerlin.configureMobilityTypeSubPopulations(config, sensitivityFactor);
 
-        //baseCase: set drt constant to very bad value
-//        {
-//            config.planCalcScore().getScoringParametersPerSubpopulation().values().forEach(scoringParameterSet -> {
-//                scoringParameterSet.getOrCreateModeParams("drt").setConstant(-100);
-//                scoringParameterSet.getOrCreateModeParams("drt_teleportation").setConstant(-100);
-//            });
-//        }
+        //this will throw an exception if more than one drt mode is configured... multiple drt operators are currently not supported with car banned scenario..
+        DrtConfigGroup drtConfigGroup = DrtConfigGroup.getSingleModeDrtConfig(config);
 
+        //prepare car banned specific parameter settings
+        CarBannedScenarioPreparation.prepareConfig(config,drtConfigGroup, Arrays.asList(WALK_ACCESS_DRT_EGRESS_MODE, DRT_ACCESS_DRT_WALK_MODE));
+
+        //prepare scenario
         Scenario scenario = RunDrtOpenBerlinScenario.prepareScenario(config);
 
         /**
@@ -82,14 +98,45 @@ public class RunBerlinScenarioWithMobilityTypesAndDrtSpeedUp {
          */
 //        PAVEMobilityTypesForBerlin.randomlyAssignMobilityTypes(scenario.getPopulation(), PAVEMobilityTypesForBerlin.getMobilityTypesWithDefaulWeights());
 
+        //ban car from drt service area
+        CarBannedScenarioPreparation.banCarFromDRTServiceArea(scenario, drtConfigGroup);
+
+        //insert vehicles for new modes
+        configureVehicleIdsForNewModes(scenario);
+
+        //prepare controler
         Controler controler = RunDrtOpenBerlinScenario.prepareControler(scenario);
         controler.addOverridingModule(new DrtSpeedUpModule());
         OptDrt.addAsOverridingModule(controler, ConfigUtils.addOrGetModule(scenario.getConfig(), MultiModeOptDrtConfigGroup.class));
 
+        //configure intermodal routing modules for new modes
+        //and bind custom main mode indentifier that can handle new modes
+        controler.addOverridingModule(new AbstractModule() {
+            @Override
+            public void install() {
+                install(new WalkAccessDRTEgress2CarModule(WALK_ACCESS_DRT_EGRESS_MODE, drtConfigGroup));
+                install(new DRTAccessWalkEgress2CarModule(DRT_ACCESS_DRT_WALK_MODE, drtConfigGroup));
+//                bind(MainModeIdentifier.class).to(OpenBerlinCarBannedFromCityAnalysisMainModeIdentifier.class);
+                bind(AnalysisMainModeIdentifier.class).to(OpenBerlinCarBannedFromCityAnalysisMainModeIdentifier.class);
+            }
+        });
+
+        //run simulation
         controler.run();
 
+        //run analysis
         RunBerlinScenario.runAnalysis(controler);
     }
 
+    private static final void configureVehicleIdsForNewModes(Scenario scenario) {
+        //add mode vehicle id's
+        scenario.getPopulation().getPersons().values().parallelStream().forEach(person -> {
+            Map<String, Id<Vehicle>> vehicleIdMap = new HashMap<>();
+            Id<Vehicle> vehicleId = Id.createVehicleId(person.getId().toString());
+            vehicleIdMap.put(WALK_ACCESS_DRT_EGRESS_MODE, vehicleId);
+            vehicleIdMap.put(DRT_ACCESS_DRT_WALK_MODE, vehicleId);
+            VehicleUtils.insertVehicleIdsIntoAttributes(person, vehicleIdMap);
+        });
+    }
 
 }
