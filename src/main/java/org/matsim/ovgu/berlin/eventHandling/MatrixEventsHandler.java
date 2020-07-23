d84db965e6838dbf0cf0796443986c393ef6ae49
koetscha <koetscha@bwl8-7.ww.uni-magdeburg.de>
2020-07-23 22:40:22 +0200

    fix tt over 1 hour bug

--- src/main/java/org/matsim/ovgu/berlin/eventHandling/MatrixEventsHandler.java
+++ src/main/java/org/matsim/ovgu/berlin/eventHandling/MatrixEventsHandler.java
@@ -3,6 +3,7 @@ package org.matsim.ovgu.berlin.eventHandling;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.matsim.api.core.v01.Id;
@@ -15,6 +16,7 @@ import org.matsim.api.core.v01.events.handler.LinkLeaveEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonArrivalEventHandler;
 import org.matsim.api.core.v01.events.handler.PersonDepartureEventHandler;
 import org.matsim.api.core.v01.network.Link;
+import org.matsim.api.core.v01.population.Person;
 
 public class MatrixEventsHandler implements LinkEnterEventHandler, LinkLeaveEventHandler, PersonArrivalEventHandler,
 		PersonDepartureEventHandler {
@@ -23,6 +25,8 @@ public class MatrixEventsHandler implements LinkEnterEventHandler, LinkLeaveEven
 	private List<Double> departures = new ArrayList<Double>();
 	private List<Id<Link>> arrivalLinks = new ArrayList<Id<Link>>();
 	private List<Id<Link>> departureLinks = new ArrayList<Id<Link>>();
+	private List<Id<Person>> arrivalPersons = new ArrayList<Id<Person>>();
+	private List<Id<Person>> departurePersons = new ArrayList<Id<Person>>();
 
 	@Override
 	public void reset(int iteration) {
@@ -41,39 +45,98 @@ public class MatrixEventsHandler implements LinkEnterEventHandler, LinkLeaveEven
 	public void handleEvent(PersonArrivalEvent event) {
 		arrivalLinks.add(event.getLinkId());
 		arrivals.add(event.getTime());
+		arrivalPersons.add(event.getPersonId());
 	}
 
 	@Override
 	public void handleEvent(PersonDepartureEvent event) {
 		departureLinks.add(event.getLinkId());
 		departures.add(event.getTime());
+		departurePersons.add(event.getPersonId());
 	}
 
 	public String getTravelTimeString() {
-		Id<Link> from = departureLinks.get(0);
-		String str = from + ";" + departureLinks.get(departureLinks.size() - 1) + ";";
+		
 
-		for (int i = 0; i < arrivals.size(); i++) {
-			if (!departureLinks.get(i).equals(arrivalLinks.get(i)) && departureLinks.get(i).equals(from)) {
-				str += (arrivals.get(i) - departures.get(i)) + ";";
-			}
-		}
+		Id<Link> departure = departureLinks.get(0);
+		Id<Link> arrival = departureLinks.get(departureLinks.size() - 1);
+		String str = departure + ";" + arrival + ";";
+		double[][] arrivalDepartures = getArrivalDepartures();
+		double[] travelTimes = new double[arrivalDepartures.length];
+		
+		for (int i = 0; i < travelTimes.length; i++)
+			str += (arrivalDepartures[i][1] - arrivalDepartures[i][0]) + ";";
 		return str;
+		
+//		Id<Link> from = departureLinks.get(0);
+//		String str = from + ";" + departureLinks.get(departureLinks.size() - 1) + ";";
+//
+//		for (int i = 0; i < arrivals.size(); i++) {
+//			if (!departureLinks.get(i).equals(arrivalLinks.get(i)) && departureLinks.get(i).equals(from)) {
+//				str += (arrivals.get(i) - departures.get(i)) + ";";
+//			}
+//		}
+//		return str;
 	}
 
 	public double[] getTravelTimeArray() {
-		Id<Link> from = departureLinks.get(0);
+		double[][] arrivalDepartures = getArrivalDepartures();
+		double[] travelTimes = new double[arrivalDepartures.length];
+		
+		for (int i = 0; i < travelTimes.length; i++)
+			travelTimes[i] = arrivalDepartures[i][1] - arrivalDepartures[i][0];
+		
+		return travelTimes;
+		
+		
+		
+//		Id<Link> from = departureLinks.get(0);
+//
+//		// 24 hours
+//		double[] travelTime = new double[24];
+//		int counter = 0;
+//
+//		for (int i = 0; i < arrivals.size(); i++) {
+//			if (!departureLinks.get(i).equals(arrivalLinks.get(i)) && departureLinks.get(i).equals(from)) {
+//				travelTime[counter++] = arrivals.get(i) - departures.get(i);
+//			}
+//		}
+//		return travelTime;
+	}
 
-		// 24 hours
-		double[] travelTime = new double[24];
-		int counter = 0;
-
-		for (int i = 0; i < arrivals.size(); i++) {
-			if (!departureLinks.get(i).equals(arrivalLinks.get(i)) && departureLinks.get(i).equals(from)) {
-				travelTime[counter++] = arrivals.get(i) - departures.get(i);
-			}
+	private double[][] getArrivalDepartures() {
+		Id<Link> departure = departureLinks.get(0);
+		Id<Link> arrival = departureLinks.get(departureLinks.size() - 1);
+		HashMap<Integer, Id<Person>> persons = getUniquePersonsHashMap();
+		HashMap<Id<Person>, Double> departureTimes = getHashMap(departure, departureLinks , departurePersons, departures);
+		HashMap<Id<Person>, Double> arrivalTimes = getHashMap(arrival, arrivalLinks, arrivalPersons, arrivals);
+		
+		// 24 hours / departure and arrival
+		double[][] result = new double[24][2];
+		
+		for (int i = 0; i< persons.size();i++) {
+			result[i][0] = departureTimes.get(persons.get(i));
+			result[i][1] = arrivalTimes.get(persons.get(i));
 		}
-		return travelTime;
+		return result;		
+	}
+
+	private HashMap<Integer, Id<Person>> getUniquePersonsHashMap() {
+		HashMap<Integer, Id<Person>> persons = new HashMap<Integer, Id<Person>>();
+		for (Id<Person> person : departurePersons)
+			if (!persons.containsValue(person))
+				persons.put(persons.size(), person);
+		return persons;
+	}
+
+	private HashMap<Id<Person>, Double> getHashMap(Id<Link> link, List<Id<Link>> links, List<Id<Person>> persons,
+			List<Double> times) {
+		HashMap<Id<Person>, Double> result = new HashMap<Id<Person>, Double>();
+		for (int i = 0; i < links.size(); i++)
+			if (links.get(i).equals(link))
+				if (!result.containsKey(persons.get(i)))
+					result.put(persons.get(i), times.get(i));
+		return result;
 	}
 
 	public void printCSV(String filePath) {
