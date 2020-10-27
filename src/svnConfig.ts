be126c408a59c0a7348bffee2593d153bb914ab0
tschlenther <schlenther@vsp.tu-berlin.de>
2020-10-27 15:51:42 +0100

    change svnConfig.ts

--- src/svnConfig.ts
+++ src/svnConfig.ts
@@ -1,28 +1,28 @@
 const config: any = {
   projects: [
+//    {
+//      name: 'Sample Visualizations',
+//      url: 'example',
+//      description: 'A few visualizations of public datasets.',
+//      svn:
+//        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/viz-examples/examples',
+//      need_password: false,
+//      thumbnail: '/thumb-examples.jpg',
+//    },
+//    {
+//      name: 'Gladbeck',
+//      url: 'gladbeck',
+//      description: 'AVÖV Projekt: Gladbeck NRW',
+//      svn:
+//        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gladbeck/avoev',
+//      need_password: false,
+//      thumbnail: '/thumb-gladbeck.jpg',
+//    },
     {
-      name: 'Sample Visualizations',
-      url: 'example',
-      description: 'A few visualizations of public datasets.',
-      svn:
-        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/viz-examples/examples',
-      need_password: false,
-      thumbnail: '/thumb-examples.jpg',
-    },
-    {
-      name: 'Gladbeck',
-      url: 'gladbeck',
-      description: 'AVÖV Projekt: Gladbeck NRW',
-      svn:
-        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gladbeck/avoev',
-      need_password: false,
-      thumbnail: '/thumb-gladbeck.jpg',
-    },
-    {
-      name: 'Public-SVN',
-      url: 'public-svn',
-      description: 'Simulation results from around the world (by country)',
-      svn: 'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries',
+      name: 'PAVE',
+      url: 'pave',
+      description: 'Simulation results',
+      svn: 'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/pave/website',
       need_password: false,
       thumbnail: '/thumbnail.png',
     },
@@ -34,22 +34,6 @@ const config: any = {
       need_password: false,
       thumbnail: '/thumb-localfiles.png',
     },
-    // {
-    //   name: 'ils4 Math Cluster',
-    //   url: 'ils4',
-    //   description: 'Mount cluster files using sshfs',
-    //   svn: 'http://localhost:8000/cluster',
-    //   need_password: false,
-    //   thumbnail: '/thumb-cluster.png',
-    // },
-    // {
-    //   name: 'Apache (Local)',
-    //   url: 'apache',
-    //   description: 'Run Apache locally',
-    //   svn: 'http://localhost',
-    //   need_password: true,
-    //   thumbnail: '/thumbnail.png',
-    // },
   ],
 }
 
