34569b4c604f1ee449adc82e347270b4239ce785
tschlenther <schlenther@vsp.tu-berlin.de>
2020-10-27 15:51:42 +0100

    change svnConfig.ts

--- src/svnConfig.ts
+++ src/svnConfig.ts
@@ -1,5 +1,23 @@
 const config: any = {
   projects: [
+    //    {
+    //      name: 'Sample Visualizations',
+    //      url: 'example',
+    //      description: 'A few visualizations of public datasets.',
+    //      svn:
+    //        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/viz-examples/examples',
+    //      need_password: false,
+    //      thumbnail: '/thumb-examples.jpg',
+    //    },
+    //    {
+    //      name: 'Gladbeck',
+    //      url: 'gladbeck',
+    //      description: 'AVÖV Projekt: Gladbeck NRW',
+    //      svn:
+    //        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gladbeck/avoev',
+    //      need_password: false,
+    //      thumbnail: '/thumb-gladbeck.jpg',
+    //    },
     {
       name: 'PAVE',
       url: 'pave',
