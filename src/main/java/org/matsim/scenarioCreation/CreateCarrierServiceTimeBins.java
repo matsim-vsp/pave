ff9a3556bea32771c49d2a546c73be00cd8a7dd8
simei94 <simonmeinhardt94@web.de>
2020-11-27 13:15:03 +0100

    added edit of carrierVehicle TimeWindow because it has to be adapted to the new time windows we set

--- src/main/java/org/matsim/scenarioCreation/CreateCarrierServiceTimeBins.java
+++ src/main/java/org/matsim/scenarioCreation/CreateCarrierServiceTimeBins.java
@@ -9,9 +9,9 @@ import org.matsim.contrib.freight.utils.FreightUtils;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.gbl.MatsimRandom;
-import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.utils.objectattributes.attributable.Attributes;
+import org.matsim.vehicles.Vehicle;
+import org.matsim.vehicles.VehicleType;
 
 import java.util.*;
 
@@ -35,12 +35,14 @@ public class CreateCarrierServiceTimeBins {
         Scenario scenario = ScenarioUtils.loadScenario(config);
         FreightUtils.loadCarriersAccordingToFreightConfig(scenario);
 
-        List<Carrier> newCarriers = createServiceTimeBinsBasedOnHomeActivities(scenario);
-        new CarrierPlanWriter(newCarriers).write(OUTPUT_CARRIERS);
+//        List<Carrier> newCarriers = createServiceTimeBinsBasedOnHomeActivities(scenario);
+        Carriers newCarriers = createServiceTimeBinsBasedOnHomeActivities(scenario);
+
+        new CarrierPlanXmlWriterV2(newCarriers).write(OUTPUT_CARRIERS);
         System.out.println("New Carrier services were written to " + OUTPUT_CARRIERS);
     }
 
-    public static List<Carrier> createServiceTimeBinsBasedOnHomeActivities(Scenario scenario) {
+    public static Carriers createServiceTimeBinsBasedOnHomeActivities(Scenario scenario) {
         //Base: all home_activity starts after 8AM
         //Time bins [h]: 8:00-11:59, 12:00-15:59, 16:00-19:59, 20:00-24:00+*
         //Absolute values [number of activity starts]: 10318, 16402, 23746, 9237; sum: 59703
@@ -50,14 +52,15 @@ public class CreateCarrierServiceTimeBins {
         final Random rnd = MatsimRandom.getLocalInstance();
 
         Carriers carriers = FreightUtils.getCarriers(scenario);
-        List<Carrier> newCarriers = new ArrayList<>();
+        Carriers newCarriers = new Carriers();
+//        List<Carrier> newCarriers = new ArrayList<>();
 
         for (Carrier carrier : carriers.getCarriers().values()) {
 
-            Carrier carrierCopy = createCarrierCopy(carrier);
+            Carrier carrierCopy = createCarrierCopy(carrier, scenario);
 
             for (CarrierService service : carrier.getServices().values()) {
-                System.out.println(service.getServiceStartTimeWindow());
+//                System.out.println(service.getServiceStartTimeWindow());
                 //TODO Service time window setter is inside of Builder method, is not reachable
                 //Waiting for an answer from KMT, until then we have to work with building a copy
                 double start = 0.;
@@ -65,39 +68,77 @@ public class CreateCarrierServiceTimeBins {
 
                 double rndDouble = rnd.nextDouble();
 
-                if(rndDouble <= 0.16) {
-                    start = 72000.;
-                    end = scenario.getConfig().qsim().getEndTime().seconds();
-                } else if(rndDouble > 0.16 && rndDouble <= 0.17) {
+                if(rndDouble <= 0.17) {
+                    //08:00 to 11:59; 0.17
                     start = 28800.;
                     end = 43199.;
-                } else if(rndDouble > 0.17 && rndDouble <= 0.27) {
+                } else if(rndDouble > 0.17 && rndDouble <= 0.44) {
+                    //12:00 to 15:59; 0.27
                     start = 43200.;
                     end = 57599.;
-                } else if(rndDouble > 0.27) {
+                }  else if(rndDouble > 0.44 && rndDouble <= 0.84) {
+                    //16:00 to 15:59; 0.4
                     start = 57600.;
                     end = 71999.;
+                } else if(rndDouble > 0.84) {
+                    //20:00 to 24:00+; 0.16
+                    start = 72000.;
+                    end = scenario.getConfig().qsim().getEndTime().seconds();
+                } else {
+                    throw new RuntimeException("Random double " + rndDouble + " does not fit into the time windows, this should not happen!");
                 }
                 TimeWindow timeWindow = TimeWindow.newInstance(start, end);
 
                 CarrierService serviceDummy = createServiceCopyWithNewTimeWindow(service, timeWindow);
                 CarrierUtils.addService(carrierCopy, serviceDummy);
+                System.out.println(serviceDummy.getServiceStartTimeWindow());
 
             }
-            newCarriers.add(carrierCopy);
+//            newCarriers.add(carrierCopy);
+            newCarriers.addCarrier(carrierCopy);
         }
 
         return newCarriers;
     }
 
-    private static Carrier createCarrierCopy(Carrier carrier) {
+    private static Carrier createCarrierCopy(Carrier carrier, Scenario scenario) {
         CarrierCapabilities cap = carrier.getCarrierCapabilities();
 
         Carrier carrierCopy = CarrierUtils.createCarrier(carrier.getId());
-        carrierCopy.setCarrierCapabilities(cap);
 
+        carrierCopy.setCarrierCapabilities(createCarrierVehicleCopyAndReplace(cap));
 
         return carrierCopy;
+    }
+
+    private static CarrierCapabilities createCarrierVehicleCopyAndReplace(CarrierCapabilities cap) {
+        Collection<CarrierVehicle> carrierVehicles = cap.getCarrierVehicles().values();
+
+        for(CarrierVehicle veh : carrierVehicles) {
+            Id<Vehicle> vehId = veh.getId();
+            Id<Link> locationId = veh.getLocation();
+            VehicleType type = veh.getType();
+            Id<VehicleType> typeId = veh.getVehicleTypeId();
+
+            //our time bins begin at 8:00, so start time should be the same
+            double startTime = 8 * 3600.;
+            //not sure which value should be set here; could be scenario.getConfig().qsim().getEndTime().seconds()
+            //but is it really realistic that people will be receiving their deliveries after 22:00?
+            //we should rather use 22:00 or 21:00; of the aforementioned, 21h is more realistic to me, but we also
+            //should consider that we probably want to provide jspirt with some flexibility
+//            double endTime = scenario.getConfig().qsim().getEndTime().seconds();
+            double endTime = 22 * 3600.;
+
+            CarrierVehicle.Builder vehicleBuilder = CarrierVehicle.Builder.newInstance(vehId, locationId);
+            vehicleBuilder.setType(type);
+            vehicleBuilder.setTypeId(typeId);
+            vehicleBuilder.setEarliestStart(startTime);
+            vehicleBuilder.setLatestEnd(endTime);
+            CarrierVehicle vehicleCopy = vehicleBuilder.build();
+            cap.getCarrierVehicles().replace(vehId, veh, vehicleCopy);
+//            CarrierUtils.addCarrierVehicle(carrierCopy, vehicleCopy);
+        }
+        return cap;
     }
 
     private static CarrierService createServiceCopyWithNewTimeWindow(CarrierService service, TimeWindow timeWindow) {
@@ -106,12 +147,12 @@ public class CreateCarrierServiceTimeBins {
         int capacityDemand = service.getCapacityDemand();
         double serviceDuration = service.getServiceDuration();
 
-        CarrierService.Builder builder = CarrierService.Builder.newInstance(serviceId, locationLinkId);
-        builder.setCapacityDemand(capacityDemand);
-        builder.setServiceDuration(serviceDuration);
-        builder.setServiceStartTimeWindow(timeWindow);
+        CarrierService.Builder serviceBuilder = CarrierService.Builder.newInstance(serviceId, locationLinkId);
+        serviceBuilder.setCapacityDemand(capacityDemand);
+        serviceBuilder.setServiceDuration(serviceDuration);
+        serviceBuilder.setServiceStartTimeWindow(timeWindow);
 
-        CarrierService carrierService = builder.build();
+        CarrierService carrierService = serviceBuilder.build();
 
         return carrierService;
     }
