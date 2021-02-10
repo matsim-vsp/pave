848b03bbe4a03884ebcbda917cd2b9ebc95ca0cf
Billy Charlton <billy@okbecause.com>
2021-02-10 19:14:27 +0100

    new isak layout for folder browser

--- src/svnConfig.ts
+++ src/svnConfig.ts
@@ -1,30 +1,13 @@
 const config: any = {
   projects: [
-//    {
-//      name: 'Sample Visualizations',
-//      url: 'example',
-//      description: 'A few visualizations of public datasets.',
-//      svn:
-//        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/viz-examples/examples',
-//      need_password: false,
-//      thumbnail: '/thumb-examples.jpg',
-//    },
-//    {
-//      name: 'Gladbeck',
-//      url: 'gladbeck',
-//      description: 'AVÖV Projekt: Gladbeck NRW',
-//      svn:
-//        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/gladbeck/avoev',
-//      need_password: false,
-//      thumbnail: '/thumb-gladbeck.jpg',
-//    },
     {
       name: 'PAVE',
       url: 'pave',
       description: 'Simulation results',
-      svn: 'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/pave/website',
+      svn:
+        'https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/projects/pave/website',
       need_password: false,
-      thumbnail: '/thumbnail.png',
+      thumbnail: '/thumb-chart.jpg',
     },
     {
       name: 'Local Files',
