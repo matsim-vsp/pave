48616fd4d107505a4af038a1c24f163a0589177a
simei94 <simonmeinhardt94@web.de>
2021-02-27 14:13:52 +0100

    path changes

--- src/main/java/org/matsim/pfav/run/RunTourPlanning.java
+++ src/main/java/org/matsim/pfav/run/RunTourPlanning.java
@@ -20,12 +20,12 @@ class RunTourPlanning {
 
     public static void main(String[] args) {
 
-        String inputDir = "C:/Users/Tilmann/tubCloud/VSP_WiMi/MA-Meinhardt/InputDRT/CarrierCreationInput/readyForTourPlanning/";
-        String carriersInputName = "carriers_2hTimeWindows_openBerlinNet_LichtenbergNord_fullDay.xml ";
+        String inputDir = "C:/Users/simon/tubCloud/Shared/MA-Meinhardt/InputDRT/CarrierCreationInput/readyForTourPlanning/";
+        String carriersInputName = "carriers_4hTimeWindows_openBerlinNet_fullDay.xml ";
         String carriersOutput = inputDir + "PLANNED/" + carriersInputName;
-        String carrierVehtypes = "C:/Users/Tilmann/tubCloud/VSP_WiMi/MA-Meinhardt/InputDRT/CarrierCreationInput/carrier_vehicleTypes.xml";
+        String carrierVehtypes = "C:/Users/simon/tubCloud/Shared/MA-Meinhardt/InputDRT/CarrierCreationInput/carrier_vehicleTypes.xml";
         String networkFile = "https://svn.vsp.tu-berlin.de/repos/public-svn/matsim/scenarios/countries/de/berlin/berlin-v5.5-10pct/input/berlin-v5.5-network.xml.gz\n";
-        String changeEventsFile = "C:/Users/Tilmann/tubCloud/VSP_WiMi/MA-Meinhardt/10pct/p2-23/p2-23.networkChangeEvents.xml.gz";
+        String changeEventsFile = "C:/Users/simon/tubCloud/Shared/MA-Meinhardt/10pct/p2-23/p2-23.networkChangeEvents.xml.gz";
         if(args.length != 0){
             inputDir = args[0];
             carriersInputName = args[1];
