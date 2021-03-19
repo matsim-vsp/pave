e9b3599e6a92eac6f2ccb6836c39ea5d07852439
Billy Charlton <charlton@vsp.tu-berlin.de>
2021-03-19 13:07:57 +0100

    front page video & animation

--- src/views/HomeIndex.vue
+++ src/views/HomeIndex.vue
@@ -1,3 +1,20 @@
+<i18n>
+en:
+  head1: 'PAVE: Potential automated vehicle systems'
+  desc1: 'Automated and connected driving is expected to have considerable potential for road traffic, especially with regard to increasing traffic safety, reducing consumption and emissions, improving traffic efficiency in the medium term, and sustainably strengthening Germany as a location for business and innovation.'
+  head2: 'The idea and the method'
+  desc2: 'The short video and interactive animation below illustrate what PAVE is all about.'
+  head3: 'Explore model results'
+  desc3: 'Click on one of the service scenarios below to explore the technical outputs:'
+  more-info: 'For more information:'
+  video: 'Video: What is PAVE?'
+  model: 'Interactive model simulation'
+de:
+  head1: 'PAVE: Potentiale automatisierter Verkehrssysteme'
+  desc1: 'Durch „Automatisiertes und vernetztes Fahren“ wird ein erhebliches Potential für den Straßenverkehr erwartet, insbesondere hinsichtlich „der Steigerung der Verkehrssicherheit, der Verbrauchs- und Emissionsreduzierung, einer mittelfristigen Verbesserung der Verkehrseffizienz und der nachhaltigen Stärkung des Wirtschafts- und Innovationsstandorts Deutschland“.'
+  more-info: 'Für weitere Informationen:'
+</i18n>
+
 <template lang="pug">
 #home
 
@@ -12,24 +29,31 @@
       .main
         .right
 
-        h1 {{ $t('pave-project') }}
-        h2.readme {{ $t('tagLine') }}
+        h2 {{ $t('head1') }}
+        p {{ $t('desc1') }}
+
+        h2 {{ $t('head2') }}
+        p {{ $t('desc2') }}
+
+        .big-thumbs
+          .project
+            iframe.yt-size(width="100%" src="https://www.youtube-nocookie.com/embed/lE9us7EoaLc" frameborder="2" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            .desc
+              p {{ $t('video') }}
+          .project
+            router-link(:to="'/v/vehicle-animation/1-robotaxi/viz-vehicles-1.yaml'")
+              img.clip-it(src="/pave/thumb-drt.jpg")
+            .desc
+              p {{ $t('model') }}
+
+        h2 {{ $t('head3') }}
+        p {{ $t('desc3') }}
 
         svn-projects.gap
 
         .readme(v-html="readme")
 
 </template>
-<i18n>
-en:
-  pave-project: 'PAVE Project'
-  more-info: 'For more information:'
-  tagLine: 'MATSim model output browser for the PAVE project.'
-de:
-  pave-project: 'PAVE Projekt'
-  more-info: 'Für weitere Informationen:'
-  tagLine: 'Hier finden Sie Ergebnisse des PAVE-Projektes '
-</i18n>
 
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
@@ -85,6 +109,53 @@ export default MyComponent
 
 .main {
   margin: 0 auto;
+}
+
+.main h2 {
+  padding-top: 2rem;
+  margin-bottom: 1rem;
+}
+
+.big-thumbs {
+  margin-top: 2rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+}
+
+.clip-it {
+  width: 100%;
+  height: 15rem;
+  object-fit: cover;
+}
+
+.yt-size {
+  height: 15rem;
+}
+
+.project {
+  flex: 1;
+  cursor: pointer;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  background-color: var(--bgBold);
+  border-radius: 16px;
+}
+
+.desc h3 {
+  color: var(--text);
+  margin: 0.5rem 0;
+  // margin-bottom: 0.25rem;
+}
+
+.project .desc {
+  padding: 0.5rem 1rem 1rem 1rem;
+}
+
+.project:hover {
+  // background-color: var(--bgHover);
+  box-shadow: var(--shadowMode);
 }
 
 .banner {
