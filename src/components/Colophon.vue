c2cfc079cfff15252056319a6823184739d01303
frie_voe <friedrich.k.voelkers@tu-berlin.de>
2021-04-11 15:33:31 +0200

    Added TODOs

--- src/components/Colophon.vue
+++ src/components/Colophon.vue
@@ -55,7 +55,7 @@ de:
 
   p.gdpr GDPR: {{ $t('gdpr')}}
 
-  p.gdpr: a(href="../../imprint") {{ $t('imprint')}}
+  p.gdpr: a(href="../../imprint") {{ $t('imprint')}} <!--TODO: Change href link-->
 
 </template>
 
